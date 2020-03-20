5a10acd466d3058ced90e4cfebf78091da31380b
AnthonyTB <anthonylol@protonmail.com>
2020-03-19 21:05:58 -0700

    worked on search component

--- client/src/Components/DashPlatforms/DashPlatforms.js
+++ client/src/Components/DashPlatforms/DashPlatforms.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './DashPlatforms.css';
 
 function DashPlatforms() {
-  return <div className='DashPlatforms'></div>;
+  return (
+    <div className='DashPlatforms'>
+      <h1>Social Platforms</h1>
+    </div>
+  );
 }
 
 export default DashPlatforms;
--- client/src/Components/DashStats/DashStats.js
+++ client/src/Components/DashStats/DashStats.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './DashStats.css';
 
 function DashStats() {
-  return <div className='DashStats'></div>;
+  return (
+    <div className='DashStats'>
+      <h1>Stats</h1>
+    </div>
+  );
 }
 
 export default DashStats;
